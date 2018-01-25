e04957242f3293d65d1c4d26a66a093b56b51f30
Jed Duffey <jedduffey@gmail.com>
2018-01-25 11:09:34 -0500

    Initial commit

---
+++ src/main/java/Product.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jedduffey on 1/25/18.
+ */
+public class Product {
+
+    private int currentPrice = 100;
+
+    public int getCurrentPrice() {
+
+        return currentPrice;
+    }
+}
---
+++ src/test/java/ProductTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jedduffey on 1/25/18.
+ */
+public class ProductTest {
+
+    @Test
+    public void productShouldHaveACurrentPrice(){
+        Product product = new Product();
+        Assert.assertEquals(100,product.getCurrentPrice());
+    }
+
+    @Test
+    public void 
+
+}
